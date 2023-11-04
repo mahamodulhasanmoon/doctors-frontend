592c67b78bc22d92a92c77aa3f96e5b99b1c3ed0
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-11-04 23:44:25 +0600

    service area done

--- src/Pages/Service/Service.jsx
+++ src/Pages/Service/Service.jsx
@@ -5,19 +5,19 @@ import LocationArea from "./LocationArea";
 
 const Service = () => {
   return (
-    <div className="mx-auto mt-12 w-full max-w-screen-2xl">
+    <div className="mx-auto container">
       {/* About section first para */}
-      <section>
-        <div className="mx-28  sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
-            <div className="max-w-lg">
+      <section className="my-1">
+        <div >
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8 my-10">
+            <div className="col-span-1">
               <h2 className="text-[3.375rem] mb-6 font-bold text-gray-900 sm:text-4xl">
               Service Area
               </h2>
               <span
                 className=" w-full text-gray-600 text-md"
                 style={{
-                  whiteSpace: "nowrap",
+                  
                   overflow: "hidden",
                   textOverflow: "ellipsis",
                 }}
@@ -48,29 +48,30 @@ const Service = () => {
         </div>
       </section>
       {/* About section dr phil para */}
-      <div className="mx-28  sm:flex items-center max-w-screen-xl">
-      <div className="sm:w-1/2 p-10 relative">
+      <div className="grid grid-cols-1 md:grid-cols-12 items-center gap-8">
+      <div className=" col-span-5 relative">
   <div className="image object-center text-center">
-  <div className="border-solid border-[#9ED7CE] border-8 top-0 left-0 w-[24rem] h-[28rem] pointer-events-none"></div>
+  <div className="border-solid border-[#010202] border-8 top-0 left-0 w-[24rem] h-[28rem] pointer-events-none"></div>
     <img src={massageImage2} alt="Company Image" className="z-10 absolute bottom-6 left-6 w-[24rem] h-[28rem]" />
    
   </div>
 </div>
-
-
-
-        <div className="sm:w-1/2 p-5 -ml-24 ">
+<div className="col-span-7  w-full">
           <div className="text">
-            <span className="text-gray-800 text-xl -ml-24 font-bold ">
+            <span className="text-gray-800 text-xl  font-bold ">
             Dr. Phil Chiropractor
             </span>
            
-            <div className="w-[799px] text-justify -ml-24 text-zinc-600 text-lg font-normal font-['Open Sans'] leading-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultrices bibendum arcu, sit amet fermentum odio placerat in. Sed nec tortor vel ex suscipit fringilla. Phasellus auctor turpis nec diam viverra, a rhoncus libero laoreet. Curabitur efficitur justo et massa commodo, non fringilla urna tincidunt. Vivamus vehicula, purus nec consectetur efficitur, purus lectus vehicula erat, ac dictum odio ante et nulla. In eu orci et ante efficitur dictum eget id purus. Integer rhoncus libero sed purus ultrices, vel congue mi efficitur.<br/>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean euismod, dui at ultricies congue, justo libero suscipit odio, in pellentesque lectus turpis in turpis. Donec ut libero vitae neque scelerisque rhoncus. Sed a ipsum non mi pellentesque malesuada id eget purus. Suspendisse sagittis, ante nec lacinia volutpat, tortor urna consequat arcu, sit amet sollicitudin quam metus vel ex. Sed vestibulum, nisl ac tincidunt condimentum, urna turpis laoreet arcu, et lacinia odio augue eu arcu.</div>
+            <div className=" text-justify  text-zinc-600 text-lg font-normal font-['Open Sans'] leading-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultrices bibendum arcu, sit amet fermentum odio placerat in. Sed nec tortor vel ex suscipit fringilla. Phasellus auctor turpis nec diam viverra, a rhoncus libero laoreet. Curabitur efficitur justo et massa commodo, non fringilla urna tincidunt. Vivamus vehicula, purus nec consectetur efficitur, purus lectus vehicula erat, ac dictum odio ante et nulla. In eu orci et ante efficitur dictum eget id purus. Integer rhoncus libero sed purus ultrices, vel congue mi efficitur.<br/>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean euismod, dui at ultricies congue, justo libero suscipit odio, in pellentesque lectus turpis in turpis. Donec ut libero vitae neque scelerisque rhoncus. Sed a ipsum non mi pellentesque malesuada id eget purus. Suspendisse sagittis, ante nec lacinia volutpat, tortor urna consequat arcu, sit amet sollicitudin quam metus vel ex. Sed vestibulum, nisl ac tincidunt condimentum, urna turpis laoreet arcu, et lacinia odio augue eu arcu.</div>
           </div>
-          <div className="w-[231px] -ml-24 mt-6 h-14 px-[30px] bg-cyan-700 rounded-lg justify-center items-center gap-2.5 inline-flex">
+          <div className="w-[231px]  mt-6 h-14 px-[30px] bg-cyan-700 rounded-lg justify-center items-center gap-2.5 inline-flex">
   <div className="text-white text-lg font-normal font-['Open Sans']">Get an Appointment</div>
 </div>
         </div>
+
+
+
+
       </div>
       <LocationArea/>
       <Faqs></Faqs>
