d60c04b03c7f5ece754f2edec063f09068f010e3
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-11-04 20:28:53 +0600

    done

--- src/Shared/Navmenu/Navmenu.jsx
+++ src/Shared/Navmenu/Navmenu.jsx
@@ -21,8 +21,8 @@ const Navmenu = () => {
   const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
   return (
-    <>
-      <div className="lg:mx-auto max-w-screen-2xl bg-cyan-700 h-20 px-5 lg:px-36 py-12 border-b border-zinc-300 flex justify-between items-center">
+    <div className="bg-cyan-700 ">
+      <div className="  h-20 container mx-auto py-12 border-b border-zinc-300 flex justify-between items-center">
         <img className="w-32 h-14" src={logo} alt="Logo" />
 
         {/* Mobile Menu Icon */}
@@ -47,7 +47,7 @@ const Navmenu = () => {
         </div>
         
         {/* Desktop Navigation */}
-        <nav className="hidden lg:flex items-center gap-10">
+        <nav className="hidden lg:flex items-center gap-8">
         <Link to='/'  className="text-white text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">Home</Link>
           <div className="flex items-center gap-1">
           <div className="relative">
@@ -134,8 +134,8 @@ const Navmenu = () => {
         )}
 
         <div className="w-full hidden lg:flex lg:w-auto mt-4 lg:mt-0 px-4 py-2 rounded-lg text-center">
-          <p className="flex items-center text-white lg:text-4xl  font-bold font-['Open Sans'] capitalize leading-[2.7rem]">
-          <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 39 39" fill="none">
+          <p className="flex items-center text-white lg:text-2xl  font-bold font-['Open Sans'] capitalize leading-[2.7rem]">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 39 39" fill="none">
   <g clipPath="url(#clip0_386_26)">
     <path d="M37.9386 28.6221L32.496 23.1795C30.5522 21.2357 27.2478 22.0133 26.4703 24.5401C25.8872 26.2896 23.9434 27.2615 22.194 26.8727C18.3065 25.9008 13.0583 20.847 12.0864 16.765C11.5033 15.0156 12.6695 13.0718 14.4189 12.4887C16.9458 11.7112 17.7233 8.40682 15.7796 6.46305L10.337 1.02048C8.78198 -0.34016 6.44946 -0.34016 5.08882 1.02048L1.39565 4.71365C-2.29752 8.6012 1.7844 18.9032 10.9201 28.0389C20.0559 37.1747 30.3579 41.4511 34.2454 37.5634L37.9386 33.8703C39.2993 32.3152 39.2993 29.9827 37.9386 28.6221Z" fill="white"/>
   </g>
@@ -151,7 +151,7 @@ const Navmenu = () => {
       </div>
     
 
-    </>
+    </div>
   );
 };
 
