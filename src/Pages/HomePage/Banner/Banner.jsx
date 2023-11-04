965e3d4088475b1a982cd56ea3df15213e6013e7
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-11-04 21:57:18 +0600

    done home section

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -3,11 +3,11 @@ import VideoPlayer from "./VideoPlayer/VideoPlayer";
 
 const Banner = () => {
   return (
-    <>    <div className="w-full   mx-auto  max-w-screen-2xl">
+    <>    <div className="w-full   mx-auto container">
  
- <div className="grid grid-cols-1 md:grid-cols-6   lg:mr-24 ">
- <div className="pt-[5rem] px-[2rem] col-span-4 flex flex-col justify-start items-center">
-    <div className="text-start lg:-ml-18 ">
+ <div className="grid grid-cols-1 md:grid-cols-12">
+ <div className="pt-[5rem] col-span-9 flex flex-col justify-start items-center">
+    <div className="text-start  w-full">
       <span className="text-neutral-800 text-3xl lg:text-5xl font-bold font-['Georgia'] leading-10 lg:leading-[4.4rem]">
         Say Goodbye to Pain and{" "}
       </span>
@@ -19,16 +19,16 @@ const Banner = () => {
         Get the pain relief you need and the life you deserve with <br/> Dr. Phils chiropractic services and experience  the benefits<br></br> of the ring dinger technique
       </p>
     </div>
-    <div className="flex justify-start self-start lg:ml-32 text-left flex-col lg:flex-row items-start gap-6 mt-6 lg:mt-4">
+    <div className="flex justify-start self-start  text-left flex-col lg:flex-row items-start gap-6 mt-6 lg:mt-4">
   <Link
     to="/book"
-    className="w-full lg:w-auto h-14 px-6 bg-cyan-700 rounded-lg flex justify-center items-center gap-2.5 text-white text-sm lg:text-lg font-normal lg:ml-0 ml-4"
+    className="w-full lg:w-auto h-14 px-6 bg-cyan-700 rounded-lg flex justify-center items-center gap-2.5 text-white text-sm lg:text-lg font-normal "
   >
     Book Appointment
   </Link>
   <Link
     to="/contact"
-    className="w-full lg:mb-0 lg:ml-0  ml-3 mb-6 lg:w-auto h-14 px-6 lg:px-[20px] rounded-lg border border-cyan-700 flex justify-center items-center gap-2.5 text-cyan-700 text-sm lg:text-lg font-normal mt-4 lg:mt-0"
+    className="w-full lg:mb-0   mb-6 lg:w-auto h-14 px-6 lg:px-[20px] rounded-lg border border-cyan-700 flex justify-center items-center gap-2.5 text-cyan-700 text-sm lg:text-lg font-normal mt-4 lg:mt-0"
   >
     Contact Us
   </Link>
@@ -38,8 +38,8 @@ const Banner = () => {
 
   </div>
 
-  <div className="col-span-1 lg:mt-12 lg:pr-24 md:col-span-2 flex justify-center items-center">
-    <div className="  md:w-[340px] bg-cyan-700 shadow-lg p-6 rounded-lg text-center">
+  <div className="col-span-1 lg:mt-12  md:col-span-3 flex justify-center items-center">
+    <div className="  bg-cyan-700 shadow-lg p-6 rounded-lg text-center">
       <div>
         <h1 className="text-xl md:text-2xl font-semibold text-white mb-4">Need Relief?</h1>
         <p className="text-xl font-semibold text-white mb-4">Reserve your free new patient consultation by booking online today!</p>
